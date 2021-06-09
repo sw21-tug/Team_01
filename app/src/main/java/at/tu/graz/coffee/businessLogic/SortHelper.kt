fa134f003103b08338d42ea0c85499b695d233f1
Laura Pessl <laura.pessl@student.tugraz.at>
2021-06-09 09:03:21 +0200

    COFFEE-041 created sorthelper [HF,PL]

---
+++ app/src/main/java/at/tu/graz/coffee/businessLogic/SortHelper.kt
@@ -0,0 +1,17 @@
+package at.tu.graz.coffee.businessLogic
+
+import at.tu.graz.coffee.model.CoffeeWithReviews
+import at.tu.graz.coffee.model.Review
+
+class SortHelper {
+    companion object {
+        fun sortCoffee(
+            coffees: List<CoffeeWithReviews>
+        ): List<CoffeeWithReviews> {
+
+            return coffees
+        }
+    }
+
+
+}
