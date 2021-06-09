73f61670e034d54b073f6ef88bec952caf3b4568
Laura Pessl <laura.pessl@student.tugraz.at>
2021-06-09 09:05:22 +0200

    COFFEE-041 deleted useless code in sorthelper [HF,PL]

--- app/src/main/java/at/tu/graz/coffee/businessLogic/SortHelper.kt
+++ app/src/main/java/at/tu/graz/coffee/businessLogic/SortHelper.kt
@@ -1,17 +1,13 @@
 package at.tu.graz.coffee.businessLogic
 
 import at.tu.graz.coffee.model.CoffeeWithReviews
-import at.tu.graz.coffee.model.Review
 
 class SortHelper {
     companion object {
         fun sortCoffee(
-            coffees: List<CoffeeWithReviews>
+                coffees: List<CoffeeWithReviews>
         ): List<CoffeeWithReviews> {
-
             return coffees.sortedByDescending { it.coffee.evaluationTotal }
         }
     }
-
-
 }
