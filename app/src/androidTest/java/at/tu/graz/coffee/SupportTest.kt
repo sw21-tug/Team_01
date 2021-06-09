b857ff35193adf0a3119fb46a8c0c8ae2d1fa1d3
goschale <a.gosch@student.tugraz.at>
2021-06-09 11:09:12 +0200

    COFFEE-040-Improved-Design Search and Support Test fixed [GA,PM]

--- app/src/androidTest/java/at/tu/graz/coffee/SupportTest.kt
+++ app/src/androidTest/java/at/tu/graz/coffee/SupportTest.kt
@@ -31,82 +31,122 @@ class SupportTest {
     @Test
     fun supportTest() {
         val appCompatImageButton = onView(
-allOf(withContentDescription("Open navigation drawer"),
-childAtPosition(
-allOf(withId(R.id.toolbar),
-childAtPosition(
-withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
-0)),
-1),
-isDisplayed()))
+            allOf(
+                withContentDescription("Open navigation drawer"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withClassName(`is`("com.google.android.material.appbar.AppBarLayout")),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatImageButton.perform(click())
-        
+
         val navigationMenuItemView = onView(
-allOf(withId(R.id.nav_support),
-childAtPosition(
-allOf(withId(R.id.design_navigation_view),
-childAtPosition(
-withId(R.id.nav_view),
-0)),
-4),
-isDisplayed()))
+            allOf(
+                withId(R.id.nav_support),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
         navigationMenuItemView.perform(click())
-        
+
         val appCompatEditText = onView(
-allOf(withId(R.id.txt_supportMsg),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_host_fragment),
-0),
-1),
-isDisplayed()))
+            allOf(
+                withId(R.id.txt_supportMsg),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.support_cardview_Message),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
         appCompatEditText.perform(replaceText("Test"), closeSoftKeyboard())
-        
-        val textView = onView(
-allOf(withId(R.id.textView2), withText("Your Message:"),
-withParent(withParent(withId(R.id.nav_host_fragment))),
-isDisplayed()))
-        textView.check(matches(withText("Your Message:")))
-        
+
+
         val radioButton = onView(
-allOf(withId(R.id.rdbtn_Owner), withText("Owner"),
-withParent(allOf(withId(R.id.radio_group),
-withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java)))),
-isDisplayed()))
+            allOf(
+                withId(R.id.rdbtn_Owner), withText("Owner"),
+                withParent(
+                    allOf(
+                        withId(R.id.radio_group),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
         radioButton.check(matches(isDisplayed()))
-        
+
         val radioButton2 = onView(
-allOf(withId(R.id.rdbtn_Support), withText("Support"),
-withParent(allOf(withId(R.id.radio_group),
-withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java)))),
-isDisplayed()))
+            allOf(
+                withId(R.id.rdbtn_Support), withText("Support"),
+                withParent(
+                    allOf(
+                        withId(R.id.radio_group),
+                        withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
         radioButton2.check(matches(isDisplayed()))
-        
+
         val editText = onView(
-allOf(withId(R.id.txt_supportMsg), withText("Test"),
-withParent(withParent(withId(R.id.nav_host_fragment))),
-isDisplayed()))
+            allOf(
+                withId(R.id.txt_supportMsg), withText("Test"),
+                withParent(withParent(withId(R.id.support_cardview_Message))),
+                isDisplayed()
+            )
+        )
         editText.check(matches(withText("Test")))
-        
+
         val editText2 = onView(
-allOf(withId(R.id.txt_supportMsg), withText("Test"),
-withParent(withParent(withId(R.id.nav_host_fragment))),
-isDisplayed()))
+            allOf(
+                withId(R.id.txt_supportMsg), withText("Test"),
+                withParent(withParent(withId(R.id.support_cardview_Message))),
+                isDisplayed()
+            )
+        )
         editText2.check(matches(withText("Test")))
-        
+
         val materialButton = onView(
-allOf(withId(R.id.btn_sendEmail), withText("Send Message"),
-childAtPosition(
-childAtPosition(
-withId(R.id.nav_host_fragment),
-0),
-3),
-isDisplayed()))
+            allOf(
+                withId(R.id.btn_sendEmail), withText("Send Message"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_host_fragment),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
         materialButton.perform(click())
-        }
-    
+    }
+
     private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
 
         return object : TypeSafeMatcher<View>() {
             override fun describeTo(description: Description) {
@@ -121,4 +161,4 @@ isDisplayed()))
             }
         }
     }
-    }
+}
